bbacaf6db256494bdd5932c9665b13004b4328ca
José Antonio García <jose.ramirez@cimat.mx>
2019-01-21 21:43:21 -0600

    se actualizan todos los tipos de variables 'verdesimportantes'

--- 4/SPI2019/app/Model.R
+++ 4/SPI2019/app/Model.R
@@ -1,41 +1,49 @@
-setwd("C:\\Users\\fou-f\\Desktop\\SPI2019\\app\\")
+setwd("C:\\Users\\fou-f\\Desktop\\MCE2\\4\\SPI2019\\app")
 dir()
+administrativas <- c(1,2,3,8,9)
+categoricas <- c(4, 5, 6, 11, 12, 18, 19, 20, 21, 28, 29, 30, 31, 35, 55,
+                 57, 63, 64, 65, 66, 67, 69, 70, 71, 72, 73, 74, 75, 76, 77,
+                 88, 89, 90, 100, 101, 111, 112, 114, 117, 130, 132, 133, 134,
+                 135, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 170)
+embarque <- 29:34
+numericas <- c(13, 15, 17, 22, 23, 24, 25, 26, 27, 32, 33, 34, 36, 37, 38, 45,
+               46, 47, 48, 49, 50, 58, 59, 60, 68, 78, 79, 80, 81, 82, 83, 84, 85,
+               86, 87, 91, 93, 94, 97, 98, 102, 106, 107, 126, 127, 128, 129)
 
-
-
-
-
+# definicion de funciones para rellenar el transformador base de la simulacion
+calcula.moda <- function(x)
+{
+    # x (character): columna de tipo caracter
+     # salida: mode de la columna
+    frecuencias <- table(x)
+    maxi <- which.max(frecuencias)
+    moda <- frecuencias[maxi]
+    return(names(moda))
+}
+calcula.mediana <- function(x)
+{
+    # x (numeric): columna de tipo numerica
+    # salida: mediana de la columna
+    mediana <- median(x)
+    return( mediana)
+}
 ###############################################
-# Datos #######################################
-datos <- read.csv("ELCHIDO.csv") # requerimos que el dataframe se llame data
-datos$tTipoAparato <- as.character(datos$tTipoAparato)
-datos$iNumFases <- as.character(datos$iNumFases)
-3#datos$iFrecuencia <- as.character(datos$iFrecuencia)
-datos$tTipoArreglo <- as.character(datos$tTipoArreglo)
-datos$tTipoOptimizador <- as.character(datos$tTipoOptimizador)
-datos$dRelTransformacion <- as.character(datos$dRelTransformacion)
-datos$bLlevaTerciario <- as.character(datos$bLlevaTerciario)
-datos$IDevCercano <- as.character(datos$IDevCercano)
-#datos$IVoltDefPor <- as.character(datos$IVoltDefPor)
-#datos$DVoltSistGen <- as.character(datos$DVoltSistGen)
-datos$dVoltajeSistAT
-datos$dVoltajeSistATInterfaz
-datos$dVoltajePrefallaRL <- as.character(datos$dVoltajePrefallaRL)
-datos$bTerciarioExt <- as.character(datos$bTerciarioExt)
-datos$bTerciarioCargado <- as.character(datos$bTerciarioCargado)
-datos$tNormaGar <- as.character(datos$tNormaGar)
-#datos$iLlevaSerie <- as.character(datos$iLlevaSerie)
-datos$MSNM <- as.character(datos$MSNM)
-#datos$dZry <- as.character(datos$dZry)
-#datos$dZsh <- as.character(datos$dZsh)
-datos2 <- datos[, c(1,2,4:8,11:16,18)]
-#################
-sort(unique(datos2$tTipoArreglo))
-###############
-sapply(datos2, class)
-entrada <- datos2[1, ]
+    # correcion de tipos de Datos y construccion del transformador base para la simulacion en la app ###########
+datos <- read.csv("dataframeVerdesPresentes.csv", stringsAsFactors = FALSE) #
+sapply(datos, class)
+    # cast de numericas a factores 'character' para que las salidas de 'predict' sean caracteres y no niveles
+datos[, categoricas] <- lapply(datos[, categoricas], function(x) as.character(x))
+datos[, embarque] <- lapply(datos[, embarque], function(x) as.character(x))
+    # construccion de transformacion base para la app
+entrada <- datos[1, ]
+entrada[1, categoricas] <- mapply(datos[, categoricas], FUN=calcula.moda)
+entrada[1, embarque] <- mapply(datos[, embarque], FUN=calcula.moda)
+entrada[1, numericas] <- mapply(datos[, numericas], FUN=calcula.mediana)
+
+datos <- datos[, -embarque]    # eliminamos las variables 'administrativas' por no ser importantes para la prediccion
+entrada <- entrada[, -embarque]
 save(entrada, file ='entrada.rdata')
-write.csv(datos2, file='ELCHIDOCORTO.csv', row.names = FALSE)
+write.csv(datos, file='VariablesVerdesCorrectas.csv', row.names = FALSE)
 ################################################
 ################## Modelos ###################
 arbol <- function(variable.predecir, data=datos2)
