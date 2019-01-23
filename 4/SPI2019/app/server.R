0be39f897d9d4a3b28b04980ac8b51f1fb4d1ebe
José Antonio García <jose.ramirez@cimat.mx>
2019-01-22 20:01:41 -0600

    s

--- 4/SPI2019/app/server.R
+++ 4/SPI2019/app/server.R
@@ -1,26 +1,15 @@
 ####################################
 #####    J. Antonio Garcia #########
 #####   jose.ramirez@cimat.mx ######
-####     Eduardo Uresti Charre     #
-####     Luis Daniel Mendoza       #
 ####################################
 library(plotly)
-library(MASS)
 library(shiny)
-library(knitr)
-library(rmarkdown)
 library(ggplot2)
-library(lubridate)
-library(leaflet)
-library(shinyLP)
-library(gapminder)
 library(ggplot2)
-library(shiny)
 theme_set(theme_bw())
 #########################################
 # Construccion del backend              #
 #########################################
-library(plotly)
 
 ####################################
 server <- function(input, output) {
@@ -28,10 +17,11 @@ server <- function(input, output) {
     load('entrada.rdata')
 
   #seleccion de datos simulados
-  output$tTipoAparatoselect <- renderPrint({ input$tTipoAparato })
-  output$iNumFasesselect <- renderPrint({ input$iNumFases })
-  output$iFrecuenciaselect <- renderPrint({ input$iFrecuencia })
+  output$Enfriamiento.i_TORElev4select <- renderPrint({ input$Enfriamiento.i_TORElev4 })
+  output$Enfriamiento.i_HSRElev4select <- renderPrint({ input$Enfriamiento.i_HSRElev4 })
+  output$Enfriamiento.i_TORElev5select <- renderPrint({ input$Enfriamiento.i_TORElev5 })
 
+  
   output$tTipoArregloselect <- renderPrint({ input$tTipoArreglo })
   output$tTipoOptimizadorselect <- renderPrint({ input$tTipoOptimizador })
   output$dVoltajeSistATselect <- renderPrint({ (input$dVoltajeSistAT) })
@@ -40,10 +30,14 @@ server <- function(input, output) {
 
   ############################################
   pronostico <- reactive({
-      if( input$tTipoAparato!='0' )
+    #variables numericas 
+      if( as.numeric(input$Enfriamiento.i_TORElev4) != 0 )
       {
-          entrada$tTipoAparato <<- input$tTipoAparato
-          entrada$iNumFases <<- predict(MODELOS[['iNumFases']],entrada, type='class' )
+        #vriables continuas
+          entrada$Enfriamiento.i_TORElev4 <<- input$Enfriamiento.i_TORElev4
+          entrada$Enfriamiento.i_TORElev5 <<- predict(MODELOS[['Enfriamiento.i_TORElev5']],entrada)
+          
+          
           entrada$tTipoArreglo <<- predict(MODELOS[['tTipoArreglo']],entrada, type='class' )
           entrada$tTipoOptimizador <<- predict(MODELOS[['tTipoOptimizador']],entrada, type='class' )
           entrada$bLlevaTerciario <<- predict(MODELOS[['bLlevaTerciario']],entrada, type='class' )
@@ -53,10 +47,14 @@ server <- function(input, output) {
 
           return(entrada)
       }
-      if( input$iNumFases!='0')
+      if( as.numeric(input$Enfriamiento.i_HSRElev4)!=0)
       {
-          entrada$iNumFases <<- input$iNumFases
-          entrada$tTipoAparato <<- predict(MODELOS[['tTipoAparato']],entrada, type='class' )
+        entrada$Enfriamiento.i_HSRElev4 <<- input$Enfriamiento.i_HSRElev4
+        #variables continuas
+        entrada$Enfriamiento.i_TORElev4 <<- predict(MODELOS[['Enfriamiento.i_TORElev4']],entrada )
+        entrada$Enfriamiento.i_TORElev5 <<- predict(MODELOS[['Enfriamiento.i_TORElev5']],entrada)
+        
+        
           entrada$tTipoArreglo <<- predict(MODELOS[['tTipoArreglo']],entrada, type='class' )
           entrada$tTipoOptimizador <<- predict(MODELOS[['tTipoOptimizador']],entrada, type='class' )
           entrada$bLlevaTerciario <<- predict(MODELOS[['bLlevaTerciario']],entrada, type='class' )
@@ -66,10 +64,16 @@ server <- function(input, output) {
 
           return(entrada)
       }
-      if( input$tTipoArreglo!='0')
+      if( as.numeric(input$Enfriamiento.i_TORElev5) != 0)
       {
-          entrada$tTipoArreglo <<- input$tTipoArreglo
-          entrada$tTipoAparato <<- predict(MODELOS[['tTipoAparato']],entrada, type='class' )
+        entrada$Enfriamiento.i_TORElev5 <<- input$Enfriamiento.i_TORElev5
+        
+        # variables continuas
+        entrada$Enfriamiento.i_TORElev4 <<- predict(MODELOS[['Enfriamiento.i_TORElev4']],entrada )
+        entrada$Enfriamiento.i_HSRElev4 <<- predict(MODELOS[['Enfriamiento.i_HSRElev4']],entrada)
+        entrada$Enfriamiento.i_TORElev5 <<- predict(MODELOS[['Enfriamiento.i_TORElev5']],entrada)
+        
+        
           entrada$iNumFases <<- predict(MODELOS[['iNumFases']],entrada, type='class' )
           entrada$tTipoOptimizador <<- predict(MODELOS[['tTipoOptimizador']],entrada, type='class' )
           entrada$bLlevaTerciario <<- predict(MODELOS[['bLlevaTerciario']],entrada, type='class' )
@@ -81,6 +85,12 @@ server <- function(input, output) {
       }
       if( input$tTipoOptimizador!='-2' )
       {
+        #variables numericas
+        entrada$Enfriamiento.i_TORElev4 <<- predict(MODELOS[['Enfriamiento.i_TORElev4']],entrada )
+        entrada$Enfriamiento.i_HSRElev4 <<- predict(MODELOS[['Enfriamiento.i_HSRElev4']],entrada)
+        entrada$Enfriamiento.i_TORElev5 <<- predict(MODELOS[['Enfriamiento.i_TORElev5']],entrada)
+        
+        
           entrada$tTipoOptimizador <<- input$tTipoOptimizador
           entrada$tTipoAparato <<- predict(MODELOS[['tTipoAparato']],entrada, type='class' )
           entrada$iNumFases <<- predict(MODELOS[['iNumFases']],entrada, type='class' )
@@ -94,6 +104,12 @@ server <- function(input, output) {
       }
       if( input$bLlevaTerciario!='-1' )
       {
+        #variables numericas
+        entrada$Enfriamiento.i_TORElev4 <<- predict(MODELOS[['Enfriamiento.i_TORElev4']],entrada )
+        entrada$Enfriamiento.i_HSRElev4 <<- predict(MODELOS[['Enfriamiento.i_HSRElev4']],entrada)
+        entrada$Enfriamiento.i_TORElev5 <<- predict(MODELOS[['Enfriamiento.i_TORElev5']],entrada)
+        
+        
           entrada$bLlevaTerciario <<- input$bLlevaTerciario
           entrada$tTipoAparato <<- predict(MODELOS[['tTipoAparato']],entrada, type='class' )
           entrada$iNumFases <<- predict(MODELOS[['iNumFases']],entrada, type='class' )
@@ -108,8 +124,13 @@ server <- function(input, output) {
       ###########segundo tab
       if( as.numeric(input$dVoltajeSistAT)!=0)
       {
-          entrada$dVoltajeSistAT <<- input$dVoltajeSistAT
-          entrada$tTipoAparato <<- predict(MODELOS[['tTipoAparato']],entrada, type='class' )
+        #variables numericas
+        entrada$Enfriamiento.i_TORElev4 <<- predict(MODELOS[['Enfriamiento.i_TORElev4']],entrada )
+        entrada$Enfriamiento.i_HSRElev4 <<- predict(MODELOS[['Enfriamiento.i_HSRElev4']],entrada)
+        entrada$Enfriamiento.i_TORElev5 <<- predict(MODELOS[['Enfriamiento.i_TORElev5']],entrada)
+        
+        
+        entrada$tTipoAparato <<- predict(MODELOS[['tTipoAparato']],entrada, type='class' )
           entrada$iNumFases <<- predict(MODELOS[['iNumFases']],entrada, type='class' )
           entrada$tTipoArreglo <<- predict(MODELOS[['tTipoArreglo']],entrada, type='class' )
           entrada$tTipoOptimizador <<- predict(MODELOS[['tTipoOptimizador']],entrada, type='class' )
@@ -121,8 +142,13 @@ server <- function(input, output) {
       }
       if( as.numeric(input$dVoltajeSistATInterfaz)!=0)
       {
-          entrada$dVoltajeSistATInterfaz <<- input$dVoltajeSistATInterfaz
-          entrada$dVoltajeSistAT <<- predict(MODELOS[['dVoltajeSistAT']],entrada )
+        #variables numericas
+        entrada$Enfriamiento.i_TORElev4 <<- predict(MODELOS[['Enfriamiento.i_TORElev4']],entrada )
+        entrada$Enfriamiento.i_HSRElev4 <<- predict(MODELOS[['Enfriamiento.i_HSRElev4']],entrada)
+        entrada$Enfriamiento.i_TORElev5 <<- predict(MODELOS[['Enfriamiento.i_TORElev5']],entrada)
+        
+        
+        entrada$dVoltajeSistAT <<- predict(MODELOS[['dVoltajeSistAT']],entrada )
           entrada$tTipoAparato <<- predict(MODELOS[['tTipoAparato']],entrada, type='class' )
           entrada$iNumFases <<- predict(MODELOS[['iNumFases']],entrada, type='class' )
           entrada$tTipoArreglo <<- predict(MODELOS[['tTipoArreglo']],entrada, type='class' )
@@ -134,8 +160,13 @@ server <- function(input, output) {
       }
       if( input$MSNM!='0')
       {
-          entrada$MSNM <<- input$MSNM
-          entrada$dVoltajeSistATInterfaz <<- predict(MODELOS[['dVoltajeSistATInterfaz']],entrada )
+        #variables numericas
+        entrada$Enfriamiento.i_TORElev4 <<- predict(MODELOS[['Enfriamiento.i_TORElev4']],entrada )
+        entrada$Enfriamiento.i_HSRElev4 <<- predict(MODELOS[['Enfriamiento.i_HSRElev4']],entrada)
+        entrada$Enfriamiento.i_TORElev5 <<- predict(MODELOS[['Enfriamiento.i_TORElev5']],entrada)
+        
+        
+        entrada$dVoltajeSistATInterfaz <<- predict(MODELOS[['dVoltajeSistATInterfaz']],entrada )
           entrada$dVoltajeSistAT <<- predict(MODELOS[['dVoltajeSistAT']],entrada )
           entrada$tTipoAparato <<- predict(MODELOS[['tTipoAparato']],entrada, type='class' )
           entrada$iNumFases <<- predict(MODELOS[['iNumFases']],entrada, type='class' )
@@ -152,11 +183,12 @@ server <- function(input, output) {
 
 
 
+#variables numericas
+  output$Enfriamiento.i_TORElev4Auto <- renderPrint({ as.character(pronostico()[1, 'Enfriamiento.i_TORElev4']) })
+  output$Enfriamiento.i_HSRElev4Auto<- renderPrint({as.character(pronostico()[1, 'Enfriamiento.i_HSRElev4'] )})
+  output$Enfriamiento.i_TORElev5 <- renderPrint({ as.character(pronostico()[, 'Enfriamiento.i_TORElev5']) })
 
-  output$tTipoAparatoAuto <- renderPrint({ as.character(pronostico()[1, 'tTipoAparato']) })
-  output$iNumFasesAuto <- renderPrint({as.character(pronostico()[1, 'iNumFases'] )})
-  output$tTipoArregloAuto <- renderPrint({ as.character(pronostico()[, 'tTipoArreglo']) })
-  output$tTipoOptimizadorAuto <- renderPrint({ as.character( pronostico()[, 'tTipoOptimizador']) })
+    output$tTipoOptimizadorAuto <- renderPrint({ as.character( pronostico()[, 'tTipoOptimizador']) })
   output$bLlevaTerciarioAuto <- renderPrint({ as.character(pronostico()[, 'bLlevaTerciario'] )})
   output$dVoltajeSistATAuto <- renderPrint({ (pronostico()[, 'dVoltajeSistAT'] )})
   output$dVoltajeSistATInterfazAuto <- renderPrint({ (pronostico()[, 'dVoltajeSistATInterfaz'] )})
--- 4/SPI2019/app/ui.R
+++ 4/SPI2019/app/ui.R
@@ -24,58 +24,56 @@ body <- dashboardBody(
   tabItems(
     tabItem(tabName = "General",
             box(   width=6, h1('Seleccion del operador'),
-                   selectInput("tTipoArreglo", label = h4("tTipoArreglo"),
-                               choices = list("NULL" = 0, "3" = 3, "4" = 4,
-                                              '5'=5, '7'=7, '10'=10, '11'= 11, '12'=12,
-                                              '14'=14, '16'=16, '17'=17, '18'=18,
-                                              '19'=19),
-                               selected = 0),
-                   fluidRow(column(3, verbatimTextOutput("tTipoArregloselect"))),
+                   #variables continuas
+                   sliderInput("Enfriamiento.i_TORElev4", label = h3("Enfriamiento.i_TORElev4"),
+                               min = 0, max = 85, value = 0),
+                   fluidRow(column(3, verbatimTextOutput("Enfriamiento.i_TORElev4select"))),
+                   
+                   sliderInput("Enfriamiento.i_HSRElev4", label = h3("Enfriamiento.i_HSRElev4"),
+                               min = 0, max = 65, value = 0),
+                   fluidRow(column(3, verbatimTextOutput("Enfriamiento.i_TORElev4select"))),
+                   
+                   sliderInput("Enfriamiento.i_TORElev5", label = h3("Enfriamiento.i_TORElev5"),
+                               min = 0, max = 65, value = 0),
+                   fluidRow(column(3, verbatimTextOutput("Enfriamiento.i_TORElev5select"))),
+                   
             ),
         box(   width=6, h1('Sugerencia automatica basada en datos'),
-               h4("tTipoAparato"),
-            fluidRow(column(6, verbatimTextOutput("tTipoAparatoAuto"))),
-             h4("iNumFases"),
-             fluidRow(column(6, verbatimTextOutput("iNumFasesAuto"))),
-             h4("tTipoArreglo"),
-             fluidRow(column(6, verbatimTextOutput("tTipoArregloAuto"))),
-             h4("tTipoOptimizador"),
-             fluidRow(column(6, verbatimTextOutput("tTipoOptimizadorAuto"))),
-             h4("bLlevaTerciario"),
-            fluidRow(column(6, verbatimTextOutput("bLlevaTerciarioAuto")))
+               #variables numericas
+               h4("Enfriamiento.i_TORElev4"),
+            fluidRow(column(6, verbatimTextOutput("Enfriamiento.i_TORElev4Auto"))),
+            
+            h4("Enfriamiento.i_HSRElev4"),
+            fluidRow(column(6, verbatimTextOutput("Enfriamiento.i_HSRElev4Auto"))),
+            
+            
+            h4("Enfriamiento.i_TORElev5"),
+            fluidRow(column(6, verbatimTextOutput("Enfriamiento.i_TORElev5Auto"))),
+            
+            
+            h4("Enfriamiento.i_TORElev4"),
+            fluidRow(column(6, verbatimTextOutput("Enfriamiento.i_TORElev4"))),
+            
+            
+            h4("Enfriamiento.i_TORElev4"),
+            fluidRow(column(6, verbatimTextOutput("Enfriamiento.i_TORElev4"))),
+            
+            
+            h4("Enfriamiento.i_TORElev4"),
+            fluidRow(column(6, verbatimTextOutput("Enfriamiento.i_TORElev4"))),
+            
+            #variables categoricas 
             )),
 
 
     tabItem(tabName = "Otros",
-            box(   width=6, h1('Seleccion del operador'),
-                   sliderInput("dVoltajeSistAT", label = h4("dVoltajeSistAT"), min = 0,
-                               max = 500, value =0),
-                   fluidRow(column(6, verbatimTextOutput("dVoltajeSistATselect"))),
-                   sliderInput("dVoltajeSistATInterfaz",
-                               label = h4("dVoltajeSistATInterfaz"), min = 0,
-                               max = 500, value =0),
-                   fluidRow(column(6, verbatimTextOutput("dVoltajeSistATInterfazselect"))),
-
-                   selectInput("MSNM", label = h4("MSNM"),
-                               choices = list("NULL" = 0, "1000" = 1000, "1100" = 1100,
-                                              '1200'=1200, '1226'=1226,
-                                              '1250'=1250, '1300'= 1300, '1400'=1400,
-                                              '1500'=1500, '1524'=1524, '1650'=1650,
-                                              '1800'=1800, '2000'=2000, '2050'=2050,
-                                              '2069'=2069, '2200'=2200, '2300'=2300,
-                                              '2350'=2350, '2438'=2438, '2500'=2500),
-                               selected = 0),
-                   fluidRow(column(3, verbatimTextOutput("MSNMselect")))
+            box(   width=6, h1('Seleccion del operador')
+                 
 
 
             ),
-            box(width=6, h1('Sugerencia automatica basada en datos'),
-                h4("dVoltajeSistAT"),
-                fluidRow(column(6, verbatimTextOutput("dVoltajeSistATAuto"))),
-                h4("dVoltajeSistATInterfaz"),
-                fluidRow(column(6, verbatimTextOutput("dVoltajeSistATInterfazAuto"))),
-                h4("MSNM"),
-                fluidRow(column(6, verbatimTextOutput("MSNMAuto")))
+            box(width=6, h1('Sugerencia automatica basada en datos')
+                
 
 
 
