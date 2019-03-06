73bcc38135759fb64ef302a6dc7502aab4df30b5
José Antonio García <jose.ramirez@cimat.mx>
2019-03-05 23:41:59 -0600

    se documenta app para el coloquio de Acatlan

--- 4/coloquioAcatlan/TalleR/TalleR/server.R
+++ 4/coloquioAcatlan/TalleR/TalleR/server.R
@@ -7,37 +7,30 @@ library(MASS)
 library(shiny)
 library(knitr)
 library(rmarkdown)
-#setwd('/home/fou/Desktop/MCE_CIMAT/Second/CienciaDeDatos/DWD')
 #########################################
 # Construccion del backend              #
 #########################################
 #lectura de datos simulados previamente
 n <- 20
-load('datos.RData')
+load('datos.RData') #discutiremos este binario en clase
 ####################################
-server <- function(input, output) {
-  #############################markdown de SVM y DWD
-  # output$SVM <- renderUI({
-  #   rmarkdown::render(input = "SVM.Rmd",
-  #                     output_format = html_document(self_contained = TRUE),
-  #                     output_file = 'SVM.html')
-  #   shiny::includeHTML('SVM.html')
-  # })
-
+server <- function(input, output) # notemos que 'server' tiene como entradas 'input', 'output'
+{
   #seleccion de datos simulados
   a <- reactive({
-    d <- input$d #seleccion de datos
-    I <- diag(rep(1, d))
-    pos <- pos[, 1:d]
-    neg <- neg[, 1:d]
-    stack <- rbind(pos,neg)
+      # La funcionalidad de este 'Closure' es recrear datos que despues usaremos para graficar
+      # esta 'Closure' se activa en automatico cuando el usuario mueve el valor de 'input$d'
+    d <- input$d # seleccion de dimension a graficas
+    I <- diag(rep(1, d)) #creamos una matriz identidad de dimension 'd'
+    pos <- pos[, 1:d] #los objetos 'pos' y 'neg' viven en el arvhico 'datos.Rdata'
+    neg <- neg[, 1:d] #tomamos submuestras de los datos generados
+    stack <- rbind(pos,neg)# las concadenamos para tener un objeto que acepte ggplot
     pos.mean <- apply(pos, 2, mean)
     neg.mean <- apply(neg, 2, mean)
-    w <- ginv(I) %*% (pos.mean - neg.mean)
+    w <- ginv(I) %*% (pos.mean - neg.mean) # calculos necesarios para definir la linea verde
     w <- w/sum(w**2)**.5 #normalizamos el vector MDP
     X <- ginv(cov(pos)) %*% (pos.mean - neg.mean)
     X <- X/sum(X**2)**.5 #normalizamos el vector que define al frontera de Bayes
-    #acos(sum(X*w))*360/(2*pi)
     stack$label <- 1
     stack$label[(n+1):(2*n)] <- -1
     Y <- X
@@ -45,7 +38,7 @@ server <- function(input, output) {
     Y[1] <- X[2]
     Y[2] <- -X[1] #elegimos un vector perpenticular a X
     Y <- Y/sum(Y**2)**.5
-    sum(Y*w)
+    sum(Y*w) #mas calculos
     M <- cbind(X, Y,  w)
     pos.proyec <- as.matrix(pos)%*%M
     neg.proyec <- as.matrix(neg)%*%M
@@ -59,50 +52,50 @@ server <- function(input, output) {
     return(b)
   })
 
-
   #construccion de scaterplot del tab DWD
   output$puntos <- renderPlotly({
-    datos <- a()
+    datos <- a() # aqui se hace invocacion de la 'closure' anterior para hacer un muestreo de los datos simulados
     proyec <- datos[[1]]
     w <- datos[[2]]
-    fit <- lm(w ~ V2, data = proyec)
-    pal <- c("purple", "orange")
-    p1 <- ggplot(data = proyec, aes(x=V2, y=V3, color=label))+geom_point() +
-      stat_function(fun = function(z){z*(w[1]/w[2])}, aes(colour = I('MDP')), size=1.5)+
+    fit <- lm(w ~ V2, data = proyec) #una regresion lineal
+    pal <- c("purple", "orange") #definimos colores
+    #construimos la grafica de scatterplot
+    p1 <- ggplot(data = proyec, aes(x=V2, y=V3, color=label)) +
+        geom_point() + stat_function(fun = function(z){z*(w[1]/w[2])}, aes(colour = I('MDP')), size=1.5) +
       geom_hline(yintercept=0, aes(colour=I('red')),     show.legend = NA)+
       ggtitle('Proyeccion en la direccion optima de Bayes y MaxDataPiling') +
       theme_minimal()+  xlab('Bayes') + ylab('') +
       scale_colour_manual(
         labels = c('-1', '+1', 'MDP'),
-        values = c("purple", "orange", "green4")
-      )+ theme(legend.title = element_blank())
-    p2 <- ggplotly(p1) #distro en bayes
-    p2 #puntos pegados a la frontera
+        values = c("purple", "orange", "green4") )+ theme(legend.title = element_blank())
+    p2 <- ggplotly(p1) # construimos el grafico ggplot a uno plotly
+    p2
   })
 
 
 
   #construccion de densidades del tab DWD
   output$Bayes <- renderPlotly({
-    a <- a()
+    a <- a() #otravez seleccionamos datos
     proyec <- a[[1]]
+    # y construimos el grafico
     p2 <- ggplot(data = proyec, aes(x=V3, fill=label, colour=label))+geom_density()+
       geom_rug(sides="b")+ggtitle('Distro en la direccion de Bayes') + theme_minimal()+
       xlab('Bayes') + ylab('') +
       scale_fill_manual(  labels = c('-1', '+1'), values = c("purple", "orange"))+
       scale_color_manual(  labels = c('-1', '+1'), values = c("purple", "orange"))+
       theme(legend.title = element_blank())
-    p2 <- ggplotly(p2) #distro en bayes
+    p2 <- ggplotly(p2)
     p2
   })
 
-  #construccion de proyecciones del tab DWD
+  #construccion de los boxplots
   output$DWD <- renderPlotly({
     a <- a()
     proyec <- a[[1]]
     x <- subset(proyec, label == '+1')
     y <- subset(proyec, label == '-1')
-    y$V3 <- abs(y$V3)#distancia al plano separador
+    y$V3 <- abs(y$V3) #distancia al plano separador
     p <- plot_ly(y = ~x$V3, type = "box",
                  line = list(color = 'rgb(255,165,0)'),
                  name = "+1") %>%
--- 4/coloquioAcatlan/TalleR/TalleR/ui.R
+++ 4/coloquioAcatlan/TalleR/TalleR/ui.R
@@ -7,67 +7,68 @@ library(shiny)
 library(plotly)
 library(knitr)
 library(rmarkdown)
-#########################################
-# Construccion de la UI                 #
-#########################################
+####################################################
+# Construccion de la UI                            #
+# esto solo es la apariencia, no el funcionamiento #
+####################################################
+
+#NOTA: el objeto 'sidebar' tambien es un 'Closure' definido en el package 'shinydaShboard'
 sidebar <- dashboardSidebar(
-  #comenzamos con el menu
+  #comenzamos con el menu que tiene el nombre y las villetas del lado derecho
+
   sidebarMenu(
+    # se define un 'menuItem' por cada villeta, los parametros definen el dibujo
+    # que se desplegara del lado izquierdo del 'dashboard'
     menuItem("CIMAT", tabName = "CIMAT", icon = icon("dashboard")),
     menuItem("DWD", icon = icon("th"), tabName = "DWD",
              badgeLabel = "nuevo", badgeColor = "green")
-
-  )
-)
-#cramos varias tabs
-body <- dashboardBody(
-  tabItems(
-    tabItem(tabName = "CIMAT",
-            h2('Distance Weighted Discrimination (DWD),'),
-            h3(' la maldicion de la dimensionalidad')
-    ),
-
-    tabItem(tabName = "DWD",
-            h2('Geo de DWD'),
-      fluidRow(
-            box(  title = '',
-                #  background = "light-blue",
-             # background = "green",
-             # solidHeader = TRUE,
-              plotlyOutput("puntos", height = 230)
-            ),
-            box(
-              title = "",
-             # background = "light-blue", solidHeader = TRUE,
-              plotlyOutput("Bayes", height = 230)
             )
-          ),
-            fluidRow(
-            box(
-              #title = "Proyección sobre la dirección MDP",
-               #solidHeader = TRUE, background = 'light-blue',
-              plotlyOutput("DWD", height = 230)
-              ),
-            fluidRow(
-            box(
-              title = "Que dimension ?",
-              "Tamaño de muestra fijo 20", br(),
-              sliderInput("d", "d", min = 2, max = 1000, step = 5, value = 2)
-            ))
-          ),
-      fluidRow(
-          box(width = 12,h3("Los datos chaparros (HDLSS) tienden asin (d al inf y n fijo) a tener una
+    ) # aqui termina la 'Closure' que construye los tabs del lado izquierdo del dashboard
+
+#cramos varias tabs
+# NOTA: todo el contenido va dentro de un 'body' que tambien es una closure
+body <- dashboardBody(
+  # 'menuItems' construye la villeta
+  # 'tabItems' es el complemento de 'menuItem'
+  tabItems(
+    # asu vez cada villeta contiene elementos
+    tabItem(tabName = "CIMAT", #nombre de la tab que debe de corresponder con el nombre en 'menuItem'
+            h2('Distance Weighted Discrimination (DWD)'), #mensajes de reyeno
+            h3(' la maldicion de la dimensionalidad')
+            ),
+     # esta es otra villeta contiene una grafica de scatterplot
+    tabItem(tabName = "DWD", #nombre del tab que se corresponde con la el nombre en 'menuItem'
+            h2('Geometria de DWD'), #un feliz comentario
+    fluidRow( #esta linea define el 'layout' define una linea (row) del ancho de toda la 'hoja' o pantalla en que se visualiza el dashboard
+          # objeto box es una caja, la podemos reyenar de algun color con parametros extras
+          box(  title = 'Datos simulados',
+                  background = "light-blue",
+                  solidHeader = TRUE,
+              plotlyOutput("puntos", height = 230)) , #este es el objeto importante de este 'box')
+          # otra vaja dentro del mismo row
+          box(title = "Frontera opt.Bayes",
+              background = "green", solidHeader = TRUE,
+              plotlyOutput("Bayes", height = 230))
+    ), # fin de row
+    fluidRow(box( title = 'Direccion MDP',
+              plotlyOutput("DWD", height = 230)),
+    fluidRow(box( title = "Que dimension ?",
+              h3("N fijo a 20"), br(),
+              sliderInput("d", "d", min = 2, max = 1000, step = 5, value = 2) #este objeto define la barrita de slider su id es 'd' se forma en la lista 'input'
+            )) ),
+      fluidRow( box(width = 12,
+                        h3("Los datos chaparros (HDLSS) tienden asin (d al inf y n fijo) a tener una
                             geometria  rigida"),
-               h5("Cuando d >>n los datos consisten en un subespacio n dimensional y la idea de trabajar en este espacio es IMPRACTICA"),
-                h6("Las nuevas observaciones se espera que aparezcan fuera de este subespacio"),
-            h6('En el contexto de microarreglos el interes recae solo en algunos genes y esta atencion es mas dificil de antener solo con algunas comb. lin. (i.e. cualquier base degerado por los datos) de los genes considerados')
-          ))
+                        h5("Cuando d >>n los datos consisten en un subespacio n dimensional y la idea de trabajar en este espacio es IMPRACTICA"),
+                        h6("Las nuevas observaciones se espera que aparezcan fuera de este subespacio"),
+                        h6('En el contexto de microarreglos el interes recae solo en algunos genes y esta atencion es mas dificil de antener solo con algunas comb. lin. (i.e. cualquier base degerado por los datos) de los genes considerados')
+                    ))
     )
 
-    )
-  )
+    )# se termina este tab
+  )#fin de body
 
-# Put them together into a dashboardPage
+# Esto inicializa el dashboard con todos lo que construimos
 dashboardPage(skin = "purple",
   dashboardHeader(title = "CIMAT Monterrey"),
   sidebar,
